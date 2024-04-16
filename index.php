7deb0acf345ab811d466687ee49d67099776e56a
NikaCrafter <nikaerevantsovi@gmail.com>
2024-04-16 21:52:08 +0300

    Add Basic divs

--- index.php
+++ index.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Birthday Timer</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css"> 
+    <style>
+        html, body {
+            height: 100%;
+        }
+    </style>
 </head>
-<body>
-
+<body class="bg-gray-200 flex justify-center items-center ">
+    <div class="bg-gray-200 border border-red-600 shadow-md p-8 flex justify-center items-center " style="height: 90vh; width: 90vw ">
+       <div class="bg-white" style="background-color:white; height: 50vh; width:50vw;">
+        <h1 class="bg-white">Hello World</h1>
+       </div>
+    </div>
 </body>
 </html>
